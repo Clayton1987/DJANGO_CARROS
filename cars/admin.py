edb6db7462b5c0abf5b3f8cbf624954619a6d1c5
Clayton1987 <claytonunip@yahoo.com.br>
2024-12-01 12:22:34 -0300

    Criando DB e admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from cars.models import Car , Brand
+
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name',)
+
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    #list_filter = ('brand', 'factory_year', 'model_year')
+    search_fields = ('model',)
+
+
+
+
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)
