ac36e93e03b22b0674c393509c50437c017e7321
Clayton1987 <claytonunip@yahoo.com.br>
2024-12-15 18:55:06 -0300

    tela de login

--- cars/forms.py
+++ cars/forms.py
@@ -33,3 +33,16 @@ class CarModelForm(forms.ModelForm):
         model = Car
         #fields = ('model', 'brand', 'factory_year', 'model_year', 'plate', 'value', 'photo' )
         fields = '__all__'
+
+    def clean_value(self):
+        #value = self.cleaned_data['value'] #VARIAÇÃO DE SELEÇÃO CAMPO
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'O valor do carro deve ser maior que R$ 20.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'O ano de fabricação deve ser maior que 1975')
+        return factory_year
