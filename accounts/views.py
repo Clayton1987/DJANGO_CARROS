0205b9224db6f8b9950a7bcb120f49d54a46daa9
Clayton1987 <claytonunip@yahoo.com.br>
2024-12-17 19:35:43 -0300

    ajustes na tela principal login e logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def register_view(request):
@@ -29,3 +29,8 @@ def login_view(request):
     else:
         login_form = AuthenticationForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('cars_list')
